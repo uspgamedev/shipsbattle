b339da7a24e5a1dabe966bcd39b79ccdf278ce44
Fernando Omar Aluani <rewasvat@uspgamedev.org>
2015-01-04 04:45:24 -0200

    Updated main so that: -HUD checks if ship no longer exists -Fixed problem with scope of Ship variables; -Implemented method to create a sphere mesh with attached material -Added a useless subhull to enemy for damage testing purposes

--- src/shipsbattle/main.cc
+++ src/shipsbattle/main.cc
@@ -7,13 +7,19 @@
 #include <shipsbattle/objects/ship.h>
 #include <shipsbattle/components/spacedust.h>
 #include <shipsbattle/components/playercontroller.h>
-
+#include <shipsbattle/components/subsystems/typedefs.h>
 #include <shipsbattle/components/hull.h>
 #include <shipsbattle/components/subsystems/subhull.h>
+
+#include <shipsbattle/components/timedlife.h>
 
 #include <btBulletDynamicsCommon.h>
 #include <OgreSceneManager.h>
 #include <OgreOverlaySystem.h>
+
+#include <OgreMeshManager.h>
+#include <OgreHardwareBufferManager.h>
+#include <OgreSubMesh.h>
 
 #include <memory>
 #include <iostream>
@@ -31,6 +37,7 @@ using std::endl;
 using shipsbattle::objects::Ship;
 using shipsbattle::components::SpaceDust;
 using shipsbattle::components::PlayerController;
+using namespace shipsbattle::components::subsystems::DecaymentFunctions;
 
 ugdk::action::mode3d::Scene3D *ourscene;
 
@@ -91,24 +98,122 @@ void CreateHUD(Ship& pla, Ship& enemy) {
     [&pla, &enemy, plaHullStats, plaArmorStats, enemyHullStats, enemyArmorStats](double dt) {
 
         auto func = [](Ship& ship, Ogre::TextAreaOverlayElement* hullStats, Ogre::TextAreaOverlayElement* armorStats) {
-            auto hull = ship.hull()->GetSubHull("MainHull");
-            double hullPerc = 100.0 * hull->hitpoints() / hull->max_hitpoints();
-            double armorPerc = 100.0 * hull->armor_rating() / hull->max_armor_rating();
-            stringstream ssh;
-            ssh.precision(2);
-            ssh.setf(std::ios::fixed, std::ios::floatfield);
-            ssh << ship->name() << " HULL: " << hullPerc << "% (" << hull->hitpoints() << "/" << hull->max_hitpoints() << ")";
-            hullStats->setCaption(ssh.str());
-            stringstream ssa;
-            ssa.precision(2);
-            ssa.setf(std::ios::fixed, std::ios::floatfield);
-            ssa << ship->name() << " ARMOR: " << armorPerc << "% (" << hull->armor_rating() << "/" << hull->max_armor_rating() << ")";
-            armorStats->setCaption(ssa.str());
+            if (ship.valid()) {
+                auto hull = ship.hull()->GetSubHull("MainHull");
+                double hullPerc = 100.0 * hull->hitpoints() / hull->max_hitpoints();
+                double armorPerc = 100.0 * hull->armor_rating() / hull->max_armor_rating();
+                stringstream ssh;
+                ssh.precision(2);
+                ssh.setf(std::ios::fixed, std::ios::floatfield);
+                ssh << ship->name() << " HULL: " << hullPerc << "% (" << hull->hitpoints() << "/" << hull->max_hitpoints() << ")";
+                hullStats->setCaption(ssh.str());
+                stringstream ssa;
+                ssa.precision(2);
+                ssa.setf(std::ios::fixed, std::ios::floatfield);
+                ssa << ship->name() << " ARMOR: " << armorPerc << "% (" << hull->armor_rating() << "/" << hull->max_armor_rating() << ")";
+                armorStats->setCaption(ssa.str());
+            }
+            else {
+                hullStats->setCaption("HULL: DESTROYED");
+                armorStats->setCaption("ARMOR: DESTROYED");
+            }
         };
-
+        
         func(pla, plaHullStats, plaArmorStats);
         func(enemy, enemyHullStats, enemyArmorStats);
     }));
+}
+
+void createSphere(const std::string& name, const float r, const int nRings=16, const int nSegments=16) {
+    Ogre::MeshPtr pSphere = Ogre::MeshManager::getSingleton().createManual(Ogre::String(name), Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+    Ogre::SubMesh *pSphereVertex = pSphere->createSubMesh();
+
+    pSphere->sharedVertexData = new Ogre::VertexData();
+    Ogre::VertexData* vertexData = pSphere->sharedVertexData;
+
+    // define the vertex format
+    Ogre::VertexDeclaration* vertexDecl = vertexData->vertexDeclaration;
+    size_t currOffset = 0;
+    // positions
+    vertexDecl->addElement(0, currOffset, Ogre::VET_FLOAT3, Ogre::VES_POSITION);
+    currOffset += Ogre::VertexElement::getTypeSize(Ogre::VET_FLOAT3);
+    // normals
+    vertexDecl->addElement(0, currOffset, Ogre::VET_FLOAT3, Ogre::VES_NORMAL);
+    currOffset += Ogre::VertexElement::getTypeSize(Ogre::VET_FLOAT3);
+    // two dimensional texture coordinates
+    vertexDecl->addElement(0, currOffset, Ogre::VET_FLOAT2, Ogre::VES_TEXTURE_COORDINATES, 0);
+    currOffset += Ogre::VertexElement::getTypeSize(Ogre::VET_FLOAT2);
+
+    // allocate the vertex buffer
+    vertexData->vertexCount = (nRings + 1) * (nSegments + 1);
+    Ogre::HardwareVertexBufferSharedPtr vBuf = Ogre::HardwareBufferManager::getSingleton().createVertexBuffer(vertexDecl->getVertexSize(0), vertexData->vertexCount, Ogre::HardwareBuffer::HBU_STATIC_WRITE_ONLY, false);
+    Ogre::VertexBufferBinding* binding = vertexData->vertexBufferBinding;
+    binding->setBinding(0, vBuf);
+    float* pVertex = static_cast<float*>(vBuf->lock(Ogre::HardwareBuffer::HBL_DISCARD));
+
+    // allocate index buffer
+    pSphereVertex->indexData->indexCount = 6 * nRings * (nSegments + 1);
+    pSphereVertex->indexData->indexBuffer = Ogre::HardwareBufferManager::getSingleton().createIndexBuffer(Ogre::HardwareIndexBuffer::IT_16BIT, pSphereVertex->indexData->indexCount, Ogre::HardwareBuffer::HBU_STATIC_WRITE_ONLY, false);
+    Ogre::HardwareIndexBufferSharedPtr iBuf = pSphereVertex->indexData->indexBuffer;
+    unsigned short* pIndices = static_cast<unsigned short*>(iBuf->lock(Ogre::HardwareBuffer::HBL_DISCARD));
+
+    float fDeltaRingAngle = (Ogre::Math::PI / nRings);
+    float fDeltaSegAngle = (2 * Ogre::Math::PI / nSegments);
+    unsigned short wVerticeIndex = 0;
+
+    // Generate the group of rings for the sphere
+    for (int ring = 0; ring <= nRings; ring++) {
+        float r0 = r * sinf(ring * fDeltaRingAngle);
+        float y0 = r * cosf(ring * fDeltaRingAngle);
+
+        // Generate the group of segments for the current ring
+        for (int seg = 0; seg <= nSegments; seg++) {
+            float x0 = r0 * sinf(seg * fDeltaSegAngle);
+            float z0 = r0 * cosf(seg * fDeltaSegAngle);
+
+            // Add one vertex to the strip which makes up the sphere
+            *pVertex++ = x0;
+            *pVertex++ = y0;
+            *pVertex++ = z0;
+
+            Ogre::Vector3 vNormal = Ogre::Vector3(x0, y0, z0).normalisedCopy();
+            *pVertex++ = vNormal.x;
+            *pVertex++ = vNormal.y;
+            *pVertex++ = vNormal.z;
+
+            *pVertex++ = (float)seg / (float)nSegments;
+            *pVertex++ = (float)ring / (float)nRings;
+
+            if (ring != nRings) {
+                // each vertex (except the last) has six indices pointing to it
+                *pIndices++ = wVerticeIndex + nSegments + 1;
+                *pIndices++ = wVerticeIndex;
+                *pIndices++ = wVerticeIndex + nSegments;
+                *pIndices++ = wVerticeIndex + nSegments + 1;
+                *pIndices++ = wVerticeIndex + 1;
+                *pIndices++ = wVerticeIndex;
+                wVerticeIndex++;
+            }
+        }; // end for seg
+    } // end for ring
+
+    // Unlock
+    vBuf->unlock();
+    iBuf->unlock();
+    // Generate face list
+    pSphereVertex->useSharedVertices = true;
+
+    // the original code was missing this line:
+    pSphere->_setBounds(Ogre::AxisAlignedBox(Ogre::Vector3(-r, -r, -r), Ogre::Vector3(r, r, r)), false);
+    pSphere->_setBoundingSphereRadius(r);
+    // this line makes clear the mesh is loaded (avoids memory leaks)
+    pSphere->load();
+
+    //material
+    Ogre::MaterialPtr mMat = Ogre::MaterialManager::getSingleton().create("AmmoSphere", "General", true);
+    mMat->getTechnique(0)->getPass(0)->setDiffuse(Ogre::ColourValue(1.0, 0.1, 0));
+    mMat->getTechnique(0)->getPass(0)->setEmissive(Ogre::ColourValue(1.0, 0.1, 0));
+    pSphereVertex->setMaterialName("AmmoSphere");
 }
 
 Ship createShip(const std::string& name) {   
@@ -136,43 +241,47 @@ int main(int argc, char* argv[]) {
     ourscene->ShowFrameStats();
     ourscene->manager()->setSkyBox(true, "Backgrounds/Nebula1");
     
-    {
-        // create Player ship
-        Ship player = createShip("Player");
-        player->AddComponent(make_shared<SpaceDust>());
-        player->AddComponent(make_shared<PlayerController>());
-        ourscene->camera()->AttachTo(*player);
-        ourscene->camera()->SetParameters(Ogre::Vector3::ZERO, 100);
-        ourscene->camera()->SetDistance(10);
+    // prepare our simple test mesh for projectiles
+    createSphere("Ammo", 0.1);
 
-        //AerOmar bounding box size: (3.07287, 1.06726, 6.79639) - bounding sphere radius: 3.88078
-        AddSubHull(player, "BallOrigin", 0.5, 0, 0, 0);
-        //AddSubHull(player, "BallUno", 1.0, 0, 0, 1.0);
-        //AddSubHull(player, "BallBottom", 0.1, 0, 0, 0.25);
-        AddSubHull(player, "BallFront", 0.2, 0, 0, 3.7);
+    // create Player ship
+    Ship player = createShip("Player");
+    player->AddComponent(make_shared<SpaceDust>());
+    player->AddComponent(make_shared<PlayerController>());
+    ourscene->camera()->AttachTo(*player);
+    ourscene->camera()->SetParameters(Ogre::Vector3::ZERO, 100);
+    ourscene->camera()->SetDistance(10);
 
-        ourscene->event_handler().AddListener<ugdk::input::KeyPressedEvent>(
-        [&player](const ugdk::input::KeyPressedEvent& ev) -> void {
-            if (ev.scancode == ugdk::input::Scancode::I) {
-                player.hull()->TakeDamage(-100, 0.2, 5.0, btVector3(0, 0, 3.9));
-            }
-            else if (ev.scancode == ugdk::input::Scancode::O) {
-                player.hull()->TakeDamage(100, 0.2, 1.0, btVector3(0, 0, 3.9));
-            }
-            else if (ev.scancode == ugdk::input::Scancode::P) {
-                player.hull()->TakeDamage(100, 0.2, 0.0, btVector3(0, 0, 3.9));
-            }
-        });
+    //AerOmar bounding box size: (3.07287, 1.06726, 6.79639) - bounding sphere radius: 3.88078
+    AddSubHull(player, "BallOrigin", 0.5, 0, 0, 0);
+    //AddSubHull(player, "BallUno", 1.0, 0, 0, 1.0);
+    //AddSubHull(player, "BallBottom", 0.1, 0, 0, 0.25);
+    AddSubHull(player, "BallFront", 0.2, 0, 0, 3.7);
 
-        // create Enemy ship
-        Ship enemy = createShip("Enemy");
-        enemy.body()->Translate(0, 0, 80);
+    ourscene->event_handler().AddListener<ugdk::input::KeyPressedEvent>(
+    [&player](const ugdk::input::KeyPressedEvent& ev) -> void {
+        if (ev.scancode == ugdk::input::Scancode::I) {
+            player.hull()->TakeDamage(-100, 0.2, 5.0, btVector3(0, 0, 3.9), CONSTANT);
+        }
+        else if (ev.scancode == ugdk::input::Scancode::O) {
+            player.hull()->TakeDamage(100, 0.2, 1.0, btVector3(0, 0, 3.9), CONSTANT);
+        }
+        else if (ev.scancode == ugdk::input::Scancode::P) {
+            player.hull()->TakeDamage(100, 0.2, 0.0, btVector3(0, 0, 3.9), CONSTANT);
+        }
+    });
 
-        CreateHUD(player, enemy);
+    // create Enemy ship
+    Ship enemy = createShip("Enemy");
+    enemy.body()->Translate(0, 0, 80);
+    AddSubHull(enemy, "Butt", 0.2, 0.0, 0.0, -3.6);
+    player->component<PlayerController>()->set_target( enemy.hull()->GetSubHull("Butt").get() );
+    //enemy->AddComponent(std::make_shared<shipsbattle::components::TimedLife>(15.0));
 
-        // push scene
-        ugdk::system::PushScene(unique_ptr<ugdk::action::Scene>(ourscene));
-    }
+    CreateHUD(player, enemy);
+
+    // push scene
+    ugdk::system::PushScene(unique_ptr<ugdk::action::Scene>(ourscene));
 
     ugdk::system::Run();
     ugdk::system::Release();
