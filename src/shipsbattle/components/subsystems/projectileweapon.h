2e5dfdc8d02ad7b7aa6386820d70fca9c3b39c81
Fernando Omar Aluani <rewasvat@uspgamedev.org>
2015-01-04 05:03:30 -0200

    Implemented base abstract Weapon subsystem class, and implementation ProjectileWeapon class to fire projectiles

---
+++ src/shipsbattle/components/subsystems/projectileweapon.h
@@ -0,0 +1,34 @@
+#ifndef SHIPSBATTLE_SUBSYSTEMS_PROJECTILEWEAPON_H
+#define SHIPSBATTLE_SUBSYSTEMS_PROJECTILEWEAPON_H
+
+#include <shipsbattle/components/subsystems/weapon.h>
+#include <shipsbattle/objects/projectilemodel.h>
+
+namespace shipsbattle {
+namespace components {
+namespace subsystems {
+
+class ProjectileWeapon : public Weapon {
+public:
+    ProjectileWeapon(const std::string& name);
+
+    bool Fire(DamageableSystem* target) override;
+
+    /** Gets the projectile model this weapon uses to fire. */
+    objects::ProjectileModel projectile() const { return projectile_; }
+    void set_projectile(const objects::ProjectileModel& model) { projectile_ = model; }
+    /** Gets the speed at which the projectile is launched from this weapon. */
+    double launching_speed() const { return launching_speed_; }
+    void set_launching_speed(double speed) { launching_speed_ = speed; }
+
+protected:
+    objects::ProjectileModel projectile_;
+    double launching_speed_;
+};
+
+
+} // namespace subsystems
+} // namespace components
+} // namespace shipsbattle
+
+#endif // SHIPSBATTLE_SUBSYSTEMS_PROJECTILEWEAPON_H
---
+++ src/shipsbattle/components/subsystems/weapon.cc
@@ -0,0 +1,67 @@
+#include <shipsbattle/components/subsystems/weapon.h>
+
+#include <ugdk/action/3D/component/body.h>
+#include <ugdk/action/3D/element.h>
+
+#include <OgreVector3.h>
+#include <BtOgreExtras.h>
+
+using ugdk::action::mode3d::component::Body;
+
+namespace shipsbattle {
+namespace components {
+namespace subsystems {
+
+Weapon::Weapon(const std::string& name)
+: PoweredSystem(name), elapsed_(0.0), cooldown_(0.5), energy_(100.0), max_energy_(100.0), firing_angle_(60.0),
+    direction_(Ogre::Vector3::UNIT_Z)
+{
+}
+
+double Weapon::NeedsRecharge() {
+    return max_energy_ - energy_;
+}
+void Weapon::OnRecharge(double energy) {
+    set_energy(this->energy() + energy);
+}
+
+bool Weapon::CanFireAt(DamageableSystem* target) {
+    if (disabled() || !activated()) return false;
+
+    auto target_pos = BtOgre::Convert::toOgre(target->world_position());
+    auto pos = BtOgre::Convert::toOgre(world_position());
+    auto dir = parent_->owner()->component<Body>()->orientation() * direction_;
+    auto toTarget = target_pos - pos;
+    dir.normalise();
+    toTarget.normalise();
+    return dir.directionEquals(toTarget, Ogre::Degree(firing_angle_));
+}
+bool Weapon::TryFire(DamageableSystem* target) {
+    if (CanFireAt(target) && elapsed_ <= 0.0) {
+        if (Fire(target)) {
+            elapsed_ = cooldown_;
+            return true;
+        }
+    }
+    return false;
+}
+
+void Weapon::set_energy(double enrgy) {
+    energy_ = enrgy;
+    if (energy_ > max_energy_) energy_ = max_energy_;
+    if (energy_ < 0.0) energy_ = 0.0;
+}
+void Weapon::set_max_energy(double max_enrgy) {
+    max_energy_ = max_enrgy;
+    if (max_energy_ < 0.0) max_energy_ = 0.0;
+    if (energy_ > max_energy_) energy_ = max_energy_;
+}
+
+void Weapon::Update(double dt) {
+    if (elapsed_ > 0.0)
+        elapsed_ -= dt;
+}
+
+} // namespace subsystems
+} // namespace components
+} // namespace shipsbattle
