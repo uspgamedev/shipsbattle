f97b0b588137f47613ba5eafa50407dea889c5a8
Fernando Omar Aluani <rewasvat@uspgamedev.org>
2015-01-31 03:53:43 -0200

    Implemented ImpulseEngine

--- src/shipsbattle/components/subsystems/impulseengine.cc
+++ src/shipsbattle/components/subsystems/impulseengine.cc
@@ -1,23 +1,44 @@
 #include <shipsbattle/components/subsystems/impulseengine.h>
 
+#include <ugdk/action/3D/component/body.h>
+#include <ugdk/action/3D/element.h>
+
+#include <BtOgreExtras.h>
+
+using ugdk::action::mode3d::component::Body;
+
 namespace shipsbattle {
 namespace components {
 namespace subsystems {
 
 ImpulseEngine::ImpulseEngine(const std::string& name)
-: PoweredSystem(name)
+    : PoweredSystem(name), spent_energy_(0.0)
 {
 }
 
 double ImpulseEngine::NeedsRecharge() {
-    return 1;
+    return spent_energy_;
 }
 void ImpulseEngine::OnRecharge(double energy) {
-    
+    spent_energy_ -= energy;
 }
 
-void ImpulseEngine::Update(double dt) {
-    
+double ImpulseEngine::current_exhaust_power() const {
+    //CHECK: perhaps also take into account stored energy?
+    return exhaust_power_ * efficiency_;
+}
+
+Ogre::Vector3 ImpulseEngine::GenerateThrust(double power, double dt) {
+    if (power > current_exhaust_power()) power = current_exhaust_power();
+    if (power < 0.0) return Ogre::Vector3::ZERO;
+
+    spent_energy_ += energy_consumption_ * dt * power / exhaust_power_;
+
+    auto body = parent()->owner()->component<Body>();
+    auto impulse = current_direction_ * power * dt;
+    auto pos = BtOgre::Convert::toOgre(position());
+    body->ApplyImpulse(impulse, pos);
+    return pos.crossProduct(impulse); //generated torque (check Bullet's apply impulse to check it out)
 }
 
 } // namespace subsystems
--- src/shipsbattle/components/subsystems/impulseengine.h
+++ src/shipsbattle/components/subsystems/impulseengine.h
@@ -17,6 +17,10 @@ public:
     virtual double NeedsRecharge() override;
     virtual void OnRecharge(double energy) override;
 
+    /** Returns the vector in which this engine can point at that is closest to (minimum angle)
+    the given direction vector. */
+    Ogre::Vector3 GetVectorClosestTo(const Ogre::Vector3& dir) { return Ogre::Vector3::ZERO; }
+
     /** Default direction the engine is pointing at. */
     Ogre::Vector3 exhaust_direction() const { return exhaust_direction_; }
     void set_exhaust_direction(const Ogre::Vector3& dir) { exhaust_direction_ = dir; }
@@ -27,6 +31,11 @@ public:
     /** Amount of thrust the engine can output at regular power levels and efficiency. */
     double exhaust_power() const { return exhaust_power_; }
     void set_exhaust_power(double power) { exhaust_power_ = power; }
+    /** Gets the current amount of thrust the engine can output, according to power levels and efficiency. */
+    double current_exhaust_power() const;
+
+    /** Current direction the engine is pointing at. Set by Motion system. */
+    Ogre::Vector3 current_direction() const { return current_direction_; }
 
 protected:
     // exhaust directions
@@ -35,8 +44,13 @@ protected:
     // exhaust power/force
     double exhaust_power_;
 
+    Ogre::Vector3 current_direction_;
+    double spent_energy_;
+
     friend class Motion;
-    virtual void Update(double dt);
+    void set_current_direction(const Ogre::Vector3& dir) { current_direction_ = dir; }
+    // Receives exhaust power (not percentage). Returns generated torque.
+    Ogre::Vector3 GenerateThrust(double power, double dt);
 };
 
 
